9824ba27f740be698714bbf029bfc085966acb28
Marwin040 <markusmarwin00@gmail.com>
2024-11-16 20:10:47 +0530

    Update autoend.py

--- AmritaXMusic/plugins/sudo/autoend.py
+++ AmritaXMusic/plugins/sudo/autoend.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from SONALI import app
-from SONALI.misc import SUDOERS
-from SONALI.utils.database import autoend_off, autoend_on
+from AmritaXMusic import app
+from AmritaXMusic.misc import SUDOERS
+from AmritaXMusic.utils.database import autoend_off, autoend_on
 
 
 @app.on_message(filters.command("autoend") & SUDOERS)
