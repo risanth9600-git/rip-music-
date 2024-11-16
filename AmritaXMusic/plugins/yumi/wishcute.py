8aee5b291342a9a942ab62449cc925c525ebcde5
Marwin040 <markusmarwin00@gmail.com>
2024-11-16 19:37:27 +0530

    Update wishcute.py

--- AmritaXMusic/plugins/yumi/wishcute.py
+++ AmritaXMusic/plugins/yumi/wishcute.py
@@ -2,9 +2,9 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import random
 import requests
-from SONALI import app 
+from AmritaXMusic import app 
 
-SUPPORT_CHAT = "THE_INCRICIBLE"
+SUPPORT_CHAT = "The_Architect04"
 
 @app.on_message(filters.command("wish"))
 async def wish(_, m):
