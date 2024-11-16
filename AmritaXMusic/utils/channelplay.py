00f8f95f0e54a0cc962578de8915aa8bb0c5e190
Marwin040 <markusmarwin00@gmail.com>
2024-11-16 15:52:59 +0530

    Update channelplay.py

--- AmritaXMusic/utils/channelplay.py
+++ AmritaXMusic/utils/channelplay.py
@@ -1,5 +1,5 @@
-from SONALI import app
-from SONALI.utils.database import get_cmode
+from AmritaXMusic import app
+from AmritaXMusic.utils.database import get_cmode
 
 
 async def get_channeplayCB(_, command, CallbackQuery):
