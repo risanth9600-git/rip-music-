a6d1c7179fe0773805ae72081ddf93f40a15ed9d
Marwin040 <markusmarwin00@gmail.com>
2024-11-16 20:28:46 +0530

    Update seeker.py

--- AmritaXMusic/plugins/misc/seeker.py
+++ AmritaXMusic/plugins/misc/seeker.py
@@ -1,7 +1,7 @@
 import asyncio
 
-from SONALI.misc import db
-from SONALI.utils.database import get_active_chats, is_music_playing
+from AmritaXMusic.misc import db
+from AmritaXMusic.utils.database import get_active_chats, is_music_playing
 
 
 async def timer():
