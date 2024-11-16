f78d6535173dc7b53ad278ac7e393df714c2214d
Marwin040 <markusmarwin00@gmail.com>
2024-11-16 17:44:28 +0530

    Update Soundcloud.py

--- AmritaXMusic/platforms/Soundcloud.py
+++ AmritaXMusic/platforms/Soundcloud.py
@@ -2,7 +2,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from SONALI.utils.formatters import seconds_to_min
+from AmritaXMusic.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
