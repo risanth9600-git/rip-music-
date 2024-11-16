7a84ed52aa669d4dd45c769bbb6162a94b15c92d
Marwin040 <markusmarwin00@gmail.com>
2024-11-16 17:57:51 +0530

    Update call.py

--- AmritaXMusic/core/call.py
+++ AmritaXMusic/core/call.py
@@ -17,9 +17,9 @@ from pytgcalls.types.input_stream.quality import HighQualityAudio, MediumQuality
 from pytgcalls.types.stream import StreamAudioEnded
 
 import config
-from SONALI import LOGGER, YouTube, app
-from SONALI.misc import db
-from SONALI.utils.database import (
+from AmritaXMusic import LOGGER, YouTube, app
+from AmritaXMusic.misc import db
+from AmritaXMusic.utils.database import (
     add_active_chat,
     add_active_video_chat,
     get_lang,
@@ -31,11 +31,11 @@ from SONALI.utils.database import (
     remove_active_video_chat,
     set_loop,
 )
-from SONALI.utils.exceptions import AssistantErr
-from SONALI.utils.formatters import check_duration, seconds_to_min, speed_converter
-from SONALI.utils.inline.play import stream_markup, telegram_markup
-from SONALI.utils.stream.autoclear import auto_clean
-from SONALI.utils.thumbnails import get_thumb
+from AmritaXMusic.utils.exceptions import AssistantErr
+from AmritaXMusic.utils.formatters import check_duration, seconds_to_min, speed_converter
+from AmritaXMusic.utils.inline.play import stream_markup, telegram_markup
+from AmritaXMusic.utils.stream.autoclear import auto_clean
+from AmritaXMusic.utils.thumbnails import get_thumb
 from strings import get_string
 
 autoend = {}
