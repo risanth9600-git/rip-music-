9a7f09eea0460720871e8e8c94cf092e1f579388
Marwin040 <markusmarwin00@gmail.com>
2024-11-17 14:31:14 +0530

    Update pause.py

--- AmritaXMusic/plugins/admins/pause.py
+++ AmritaXMusic/plugins/admins/pause.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from SONALI import app
-from SONALI.core.call import RAUSHAN
-from SONALI.utils.database import is_music_playing, music_off
-from SONALI.utils.decorators import AdminRightsCheck
-from SONALI.utils.inline import close_markup
+from AmritaXMusic import app
+from AmritaXMusic.core.call import RAUSHAN
+from AmritaXMusic.utils.database import is_music_playing, music_off
+from AmritaXMusic.utils.decorators import AdminRightsCheck
+from AmritaXMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
