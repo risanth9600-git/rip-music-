c22aa7b9b49b9047d4450e6efce30f7a74d5f036
Marwin040 <markusmarwin00@gmail.com>
2024-11-17 15:49:57 +0530

    Update sticker.py

--- AmritaXMusic/plugins/tools/sticker.py
+++ AmritaXMusic/plugins/tools/sticker.py
@@ -3,7 +3,7 @@ import httpx
 import os
 from pyrogram import filters
 from config import BOT_USERNAME
-from SONALI import app
+from AmritaXMusic import app
 from pyrogram import filters
 import pyrogram
 from uuid import uuid4
