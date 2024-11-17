d2eecd2bc5ff69139d4d59a19b04f476aa746f3b
Marwin040 <markusmarwin00@gmail.com>
2024-11-17 17:57:53 +0530

    Update Telegram.py

--- AmritaXMusic/platforms/Telegram.py
+++ AmritaXMusic/platforms/Telegram.py
@@ -6,8 +6,8 @@ from typing import Union
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Voice
 
 import config
-from AviaxMusic import app
-from AviaxMusic.utils.formatters import (
+from AmritaXMusic import app
+from AmritaXMusic.utils.formatters import (
     check_duration,
     convert_bytes,
     get_readable_time,
