11a5aeb83b2d695726f07c91c06eef188e368eea
Marwin040 <markusmarwin00@gmail.com>
2024-11-17 15:49:13 +0530

    Update telegraph.py

--- AmritaXMusic/plugins/tools/telegraph.py
+++ AmritaXMusic/plugins/tools/telegraph.py
@@ -1,7 +1,7 @@
 import os
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from SONALI import app
+from AmritaXMusic import app
 import requests
 
 
