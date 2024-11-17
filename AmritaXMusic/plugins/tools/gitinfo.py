c31b6bae1dcd4c8173c103721b219b9927b6aa1f
Marwin040 <markusmarwin00@gmail.com>
2024-11-17 16:04:13 +0530

    Update gitinfo.py

--- AmritaXMusic/plugins/tools/gitinfo.py
+++ AmritaXMusic/plugins/tools/gitinfo.py
@@ -1,7 +1,7 @@
 import asyncio, os, time, aiohttp
 import aiohttp
 from pyrogram import filters
-from SONALI import app
+from AmritaXMusic import app
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
