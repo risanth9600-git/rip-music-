875c3f080a8c96d850142e7973f33baf58a7d90f
Markus Marwin <markusmarwin00@gmail.com>
2024-11-18 10:35:07 +0530

    Update settings.py

--- AmritaXMusic/plugins/bot/settings.py
+++ AmritaXMusic/plugins/bot/settings.py
@@ -54,7 +54,7 @@ async def settings_mar(client, message: Message, _):
 @languageCB
 async def gib_repo(client, CallbackQuery, _):
     await CallbackQuery.edit_message_media(
-        InputMediaVideo("https://envs.sh/50K.mp4"),
+        InputMediaVideo("https://envs.sh/5DB.mp4"),
         reply_markup=InlineKeyboardMarkup(
             [[InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data=f"settingsback_helper")]]
         ),
