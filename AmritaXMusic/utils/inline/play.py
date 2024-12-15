85b6b5c09aac5570fe2315b73257a8deaabe6c24
Akash <126413936+crazyworld-izzy@users.noreply.github.com>
2024-12-16 03:28:48 +0530

    Update play.py

--- AmritaXMusic/utils/inline/play.py
+++ AmritaXMusic/utils/inline/play.py
@@ -40,25 +40,25 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "𝁘𝃜𝆆♡〭𝀈—————————"
+        bar = "𝃜𝆆♡〭—————————"
     elif 10 < umm < 20:
-        bar = "—𝁘𝃜𝆆♡〭𝀈————————"
+        bar = "—𝃜𝆆♡〭————————"
     elif 20 <= umm < 30:
-        bar = "——𝁘𝃜𝆆♡〭𝀈———————"
+        bar = "——𝃜𝆆♡〭———————"
     elif 30 <= umm < 40:
-        bar = "———𝁘𝃜𝆆♡〭𝀈——————"
+        bar = "———𝃜𝆆♡〭——————"
     elif 40 <= umm < 50:
-        bar = "————𝁘𝃜𝆆♡〭𝀈—————"
+        bar = "————𝃜𝆆♡〭—————"
     elif 50 <= umm < 60:
-        bar = "—————𝁘𝃜𝆆♡〭𝀈————"
+        bar = "—————𝃜𝆆♡〭————"
     elif 60 <= umm < 70:
-        bar = "——————𝁘𝃜𝆆♡〭𝀈———"
+        bar = "——————𝃜𝆆♡〭———"
     elif 70 <= umm < 80:
-        bar = "———————𝁘𝃜𝆆♡〭𝀈——"
+        bar = "———————𝃜𝆆♡〭——"
     elif 80 <= umm < 95:
-        bar = "————————𝁘𝃜𝆆♡〭𝀈—"
+        bar = "————————𝃜𝆆♡〭—"
     else:
-        bar = "—————————𝁘𝃜𝆆♡〭𝀈"
+        bar = "—————————𝃜𝆆♡〭"
 
     buttons = [
         [
@@ -75,12 +75,12 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
             )
         ],
            [
-            InlineKeyboardButton(text="𝁘𝃜𝆆♡〭𝀈  Dᴇᴠᴇʟᴏᴘᴇʀ 𝁘𝃜𝆆♡〭𝀈", url=f"https://t.me/Itz_alpha_dude"),
-            InlineKeyboardButton(text="𝁘𝃜𝆆♡〭𝀈  ᴏᴡɴᴇʀ 𝁘𝃜𝆆♡〭𝀈", url=f"https://t.me/OwnerofTheDenki"),           
+            InlineKeyboardButton(text="𝃜𝆆♡〭  Dᴇᴠᴇʟᴏᴘᴇʀ 𝃜𝆆♡〭", url=f"https://t.me/Itz_alpha_dude"),
+            InlineKeyboardButton(text="𝃜𝆆♡〭  ᴏᴡɴᴇʀ 𝃜𝆆♡〭", url=f"https://t.me/OwnerofTheDenki"),           
         ],
         [
-        InlineKeyboardButton(text="𝁘𝃜𝆆♡〭𝀈  ᴜᴘᴅᴀᴛᴇ 𝁘𝃜𝆆♡〭𝀈", url=f"https://t.me/denkimusic"),
-        InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="𝁘𝃜𝆆♡〭𝀈  close 𝁘𝃜𝆆♡〭𝀈"),
+        InlineKeyboardButton(text="𝃜𝆆♡〭  ᴜᴘᴅᴀᴛᴇ 𝃜𝆆♡〭", url=f"https://t.me/denkimusic"),
+        InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="𝃜𝆆♡〭 close 𝃜𝆆♡〭"),
         ]
     ]
 
@@ -97,12 +97,12 @@ def stream_markup(_, videoid, chat_id):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
          ],
              [
-            InlineKeyboardButton(text="𝁘𝃜𝆆♡〭𝀈  Dᴇᴠᴇʟᴏᴘᴇʀ 𝁘𝃜𝆆♡〭𝀈", url=f"https://t.me/Itz_alpha_dude"),
-            InlineKeyboardButton(text="𝁘𝃜𝆆♡〭𝀈  ᴏᴡɴᴇʀ 𝁘𝃜𝆆♡〭𝀈", url=f"https://t.me/OwnerofTheDenki"),           
+            InlineKeyboardButton(text="𝃜𝆆♡〭 Dᴇᴠᴇʟᴏᴘᴇʀ 𝃜𝆆♡〭", url=f"https://t.me/Itz_alpha_dude"),
+            InlineKeyboardButton(text="𝃜𝆆♡〭  ᴏᴡɴᴇʀ 𝃜𝆆♡〭", url=f"https://t.me/OwnerofTheDenki"),           
         ],
         [
-        InlineKeyboardButton(text="𝁘𝃜𝆆♡〭𝀈  ᴜᴘᴅᴀᴛᴇ 𝁘𝃜𝆆♡〭𝀈", url=f"https://t.me/denkimusic"),
-        InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="𝁘𝃜𝆆♡〭𝀈  close 𝁘𝃜𝆆♡〭𝀈"),
+        InlineKeyboardButton(text="𝃜𝆆♡〭  ᴜᴘᴅᴀᴛᴇ 𝃜𝆆♡〭", url=f"https://t.me/denkimusic"),
+        InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="𝃜𝆆♡〭  close 𝃜𝆆♡〭"),
         ]
     ]
     return buttons
